36d8e963c06a851a6917cd52a6e370e9b29436d9
JonasDreessen <dreessenjonas95@gmail.com>
2019-07-01 14:03:12 +0200

    javascript and axios script

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
-var apiCall = 'api.openweathermap.org/data/2.5/forecast?q={city name},{country code}'
+// 1283710
 
-// API key = 919c5edf6c3606dd2160fcd891f6a289
+/* document.querySelector('.city').addEventListener('keypress', function (e) {
+  var key = e.which || e.keyCode;
+  if (key === 13) {
+    var cityInput = document.querySelector('.city').value;
+    weatherBalloon(cityInput);
+  }
+});
+
+
+
+function weatherBalloon(cityInput) {
+var key = '919c5edf6c3606dd2160fcd891f6a289';
+const city = document.querySelector(".city").value
+ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&&APPID=${key}`)
+    .then(function (response) {
+      return response.json()
+    }) 
+    // Convert data to json
+    .then(function (data) {
+      getWeatherOnScreen(data);
+    })
+}
+
+function getWeatherOnScreen(d) {
+  var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+
+  document.getElementById('description').innerHTML = d.weather[0].description;
+  document.getElementById('temp').innerHTML = celcius + '&deg;';
+  document.getElementById('location').innerHTML = d.name;
+
+} */
+
+const key = '919c5edf6c3606dd2160fcd891f6a289';
+var input = document.querySelector('.city');
+
+console.log(input);
