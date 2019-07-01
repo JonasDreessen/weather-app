7b0245ca3d4ebcd6d6da8353e3977d06df5969c8
JonasDreessen <dreessenjonas95@gmail.com>
2019-07-01 14:38:59 +0200

    add constructive data instead of the whole json file

--- js/main.js
+++ js/main.js
@@ -1,38 +1,23 @@
-// 1283710
-
-/* document.querySelector('.city').addEventListener('keypress', function (e) {
-  var key = e.which || e.keyCode;
-  if (key === 13) {
-    var cityInput = document.querySelector('.city').value;
-    weatherBalloon(cityInput);
-  }
-});
-
-
-
-function weatherBalloon(cityInput) {
-var key = '919c5edf6c3606dd2160fcd891f6a289';
-const city = document.querySelector(".city").value
- fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&&APPID=${key}`)
-    .then(function (response) {
-      return response.json()
-    }) 
-    // Convert data to json
-    .then(function (data) {
-      getWeatherOnScreen(data);
-    })
-}
-
-function getWeatherOnScreen(d) {
-  var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
-
-  document.getElementById('description').innerHTML = d.weather[0].description;
-  document.getElementById('temp').innerHTML = celcius + '&deg;';
-  document.getElementById('location').innerHTML = d.name;
-
-} */
-
 const key = '919c5edf6c3606dd2160fcd891f6a289';
-var input = document.querySelector('.city');
+const input = document.querySelector('.city');
+const button = document.querySelector('.submit-city');
 
-console.log(input);
+
+function getWeather(){
+  button.addEventListener('click', async function(){
+    let city = input.value;
+    let api = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&&APPID=${key}`;
+    let response = await axios.get(api);
+    displayWeather( response );
+    
+  })
+}
+getWeather();
+
+function displayWeather( response ) {
+	var celcius = Math.round(parseFloat(response.main.temp)-273.15);
+	
+	document.getElementById('description').innerHTML = response.weather[0].description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = response.name;
+}
